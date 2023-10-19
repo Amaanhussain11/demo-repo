c261a5ef45652492006b96e3e742bcdb23d0ee3c
Amaan Hussain <amaanhussain983@gmail.com>
2023-10-19 15:58:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+const App = () => (
+  <>
+    <div
+      className="container"
+    >Hello, world</div>
+  </>
+);
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["src\App.jsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  }
+}
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
